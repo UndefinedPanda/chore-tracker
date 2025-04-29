77763c55dab07c8fbbd76251d5230455be252f24
dalton <daltonpineault@gmail.com>
2025-04-29 19:10:34 -0400

    update

--- src/routes/chores.ts
+++ src/routes/chores.ts
@@ -1,14 +1,53 @@
 import { Router, Request, Response } from 'express';
 import { PrismaClient } from '../../prisma/generated/prisma';
-import { PrismaClientKnownRequestError } from '../../prisma/generated/prisma/runtime/library';
+import { PrismaClientKnownRequestError, RawQueryArgs } from '../../prisma/generated/prisma/runtime/library';
 
 const prisma = new PrismaClient();
 
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const children = await prisma.child.findMany();
-    res.status(200).send(children);
+
+});
+
+router.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const chore = await prisma.chore.findFirst({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        if (!chore) {
+            res.status(404).send('No chore found');
+            return;
+        }
+
+        res.status(200).json(chore);
+
+    } catch (err) {
+        res.sendStatus(500);
+    }
+});
+
+router.get('/:childId', async (req: Request, res: Response) => {
+    try {
+        const chores = await prisma.chore.findMany({
+            where: {
+                id: Number(req.params.childId)
+            }
+        });
+
+        if (!chores) {
+            res.status(404).send('No chore found');
+            return;
+        }
+
+        res.status(200).json(chores);
+
+    } catch (err) {
+        res.sendStatus(500);
+    }
 });
 
 router.post('/', async (req: Request, res: Response) => {
@@ -35,6 +74,31 @@ router.post('/', async (req: Request, res: Response) => {
             res.status(404).send(err.meta?.cause);
         }
         console.log(err);
+    }
+
+
+});
+
+router.put('/:id', async (req: Request, res: Response) => {
+    const { finished } = req.body;
+
+    if (!finished) {
+        res.status(400).send('Must have a finished field');
+        return;
+    }
+
+    try {
+        await prisma.chore.update({
+            where: {
+                id: Number(req.params.id),
+            },
+            data: {
+                finished
+            }
+        });
+
+    } catch (err) {
+        res.status(500).send('Something went wrong');
     }
 
 
